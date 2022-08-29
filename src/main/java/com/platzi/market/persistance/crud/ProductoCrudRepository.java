8780ff425b9be507d071d81c08571f676a7ff847
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-08-29 17:23:12 -0500

    Query Methods

--- src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
@@ -1,10 +1,16 @@
 package com.platzi.market.persistance.crud;
 
 import com.platzi.market.persistance.entity.Producto;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
 
-
+    //@Query(value = "SELECT * FROM PRODUCTOS WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 
 }
