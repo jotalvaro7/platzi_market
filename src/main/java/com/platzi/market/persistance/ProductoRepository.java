c373aa619cf0b0464f5755b092a809f76730b063
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-08-31 10:38:27 -0500

    implementando metodos y la anotacion @Repository

--- src/main/java/com/platzi/market/persistance/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistance/ProductoRepository.java
@@ -2,12 +2,16 @@ package com.platzi.market.persistance;
 
 import com.platzi.market.persistance.crud.ProductoCrudRepository;
 import com.platzi.market.persistance.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
@@ -20,6 +24,18 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(Long idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Long idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 
 }
