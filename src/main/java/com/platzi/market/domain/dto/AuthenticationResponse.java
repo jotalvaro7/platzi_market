96fcea4330101ed95d2417ad0c94e95f92c9af5c
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-09-02 12:16:51 -0500

    Generando un JWT

---
+++ src/main/java/com/platzi/market/domain/dto/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.platzi.market.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthenticationResponse {
+    private String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+}
